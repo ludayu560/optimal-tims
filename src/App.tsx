f86751ad24d15bb6edafe47f0cb5b48c87db6762
LuDa Yu <ludayu560@gmail.com>
2022-10-09 20:39:17 -0400

    basic site with core functionality and font. first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// get timsStatus
+var timsStatus: string = "bad"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="displayGrid">
+        <h1>It's a <span className={timsStatus}>{timsStatus}</span> time to go to DC Tims</h1>
+      </div>
     </div>
   );
 }
